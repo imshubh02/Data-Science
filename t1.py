6046c679985f7b34a6c9270ff8cdcd4d92890fb5
Shubhra <imshubhra230@gmail.com>
2023-07-26 19:00:50 +0530

    update 1

---
+++ t1.py
@@ -0,0 +1,19 @@
+#for creating septagon
+from turtle import *
+
+for i in range(7):
+    fd(120)
+    lt(51.4285)
+
+hideturtle()
+mainloop()
+
+
+
+
+
+
+
+
+
+
---
+++ t2.py
@@ -0,0 +1,9 @@
+#for creating octagon
+from turtle import *
+
+for i in range(8):
+    fd(120)
+    lt(45)
+
+hideturtle()
+mainloop()
