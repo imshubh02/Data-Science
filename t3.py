cb29705b789e93219cb20761142041765732d58d
Shubhra <imshubhra230@gmail.com>
2023-07-26 19:47:38 +0530

    for loop

---
+++ t3.py
@@ -0,0 +1,27 @@
+from turtle import *
+
+pensize(2)
+speed('fastest')
+bgcolor('black')
+pencolor('white')
+
+for i in range(6):
+    lt(60)
+    fd(100)
+    for i in range(6):
+        lt(60)
+        fd(50)
+        for i in range(6):
+            lt(60)
+            fd(25)
+            fillcolor('pink')
+            begin_fill()
+            for i in range(6):
+                lt(60)
+                fd(10)
+            end_fill()
+            circle(100, 360, 30)
+
+    
+hideturtle()
+mainloop()
