5fc62b215f783535b56b29cab0c5ca3ae770599f
Shubhra <imshubhra230@gmail.com>
2023-10-02 11:18:49 +0530

    project

--- Mini_Project/face_detection.py
+++ Mini_Project/face_detection.py
@@ -4,11 +4,12 @@ mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 
 # For static images:
-IMAGE_FILES = []
+image_files = []
 with mp_face_detection.FaceDetection(
     model_selection=1, min_detection_confidence=0.5) as face_detection:
-  for idx, file in enumerate(IMAGE_FILES):
+  for idx, file in enumerate(image_files):
     image = cv2.imread(file)
+   
     # Convert the BGR image to RGB and process it with MediaPipe Face Detection.
     results = face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
@@ -17,40 +18,32 @@ with mp_face_detection.FaceDetection(
       continue
     annotated_image = image.copy()
     for detection in results.detections:
-      print('Nose tip:')
       print(mp_face_detection.get_key_point(
-          detection, mp_face_detection.FaceKeyPoint.NOSE_TIP))
+          detection, mp_face_detection.FaceKeyPoint.MOUTH))
       mp_drawing.draw_detection(annotated_image, detection)
-    cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
-
+    
 # For webcam input:
-cap = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0)
 with mp_face_detection.FaceDetection(
     model_selection=0, min_detection_confidence=0.5) as face_detection:
-  while cap.isOpened():
-    success, image = cap.read()
+  while cam.isOpened():
+    success, image = cam.read()
     if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
       continue
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = face_detection.process(image)
-
+  
     # Draw the face detection annotations on the image.
-    image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.detections:
       for detection in results.detections:
         mp_drawing.draw_detection(image, detection)
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Face Detection', cv2.flip(image, 1))
+    
+    cv2.imshow('MediaPipe Face Detection', image)
     key = cv2.waitKey(10)
-    if key == 27:
+    if key == 27: 
       break
-      
-cap.release()
+
+cam.release()
 cv2.destroyAllWindows()
